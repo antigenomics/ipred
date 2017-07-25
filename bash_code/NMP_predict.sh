d1473858720fe864d3e64afa496759b69c07cc0b
seaneuron <seaneuron@gmail.com>
2017-07-25 14:47:48 +0300

    Bash script was fixed again

--- bash_code/NMP_predict.sh
+++ bash_code/NMP_predict.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-usage="$(basename "$0") [-h] [-s] [-a] -- a programme to run the netMHCpan in parallel
+usage="$(basename "$0") [-h] [-s] [-a] [-m] -- a programme to run the netMHCpan in parallel
 
 where:
     -h  show this help text
     -s  split file into 5000 line chunks
-    -a  set \"-a\" argument in netMHCpan (default: HLA-A02:01)"
+    -a  set \"-a\" argument in netMHCpan (default: HLA-A02:01)
+    -m  use it on mihuge server"
 
 splitfile=false
 HLA="HLA-A02:01"
+mihuge=false
 
 while getopts ":hsa:" opt; do
   case $opt in
@@ -23,6 +25,9 @@ while getopts ":hsa:" opt; do
       HLA=$OPTARG
       echo " \"-a\" argument of netMHCpan was set to $HLA"  >&2
       ;;
+    m)
+      mihuge=true
+      echo " Script runs under mihuge server"
     \?)
       echo " Invalid option: -$OPTARG" >&2
       ;;
@@ -51,5 +56,12 @@ fi
 # Parallel is used to increase the computational speed.
 
 cd tmp/
-parallel --eta netMHCpan -p -a "$HLA" ::: *.txt > "../ $HLA _NMP.txt"
+if [ "$mihuge"==true ]
+then
+  parallel --eta /home/vcvetkov/Tools/netMHCpan-3.0/bin/netMHCpan \
+ -p -a "$HLA" ::: *.txt > "../ $HLA _NMP.txt"
+else
+  parallel --eta netMHCpan -p -a "$HLA" ::: *.txt > "../ $HLA _NMP.txt"
+fi
 
+echo " The $HLA peptide binding has been predicted succesfully!"
