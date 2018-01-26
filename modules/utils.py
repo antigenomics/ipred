1610a931287c1a2eba6f71bbfadee170e94238a9
seaneuron <seaneuron@gmail.com>
2018-01-26 15:23:23 +0300

    Conducted B35:01 and B35:08 binders comparison

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from mhcflurry import predict
-from mhcflurry import class1_allele_specific
+import numpy as np
+import seaborn as sns
+from scipy import stats
+# from mhcflurry import predict
+# from mhcflurry import class1_allele_specific
 
 def coding(col, codeDict):
     colCoded = pd.Series(col, copy=True)
@@ -31,3 +34,33 @@ def make_predictions(df):
     print("Number of warnings is {}".format(warnings))
     print(warning)
     return predictions, warning_loc
+
+def count_freq(filepath):
+    aa = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'H': 6, 'I': 7,\
+      'K': 8, 'L': 9, 'M': 10, 'N': 11, 'P': 12, 'Q': 13, 'R': 14,\
+      'S': 15, 'T': 16, 'V': 17, 'W': 18, 'Y': 19}
+    data = pd.read_csv(filepath, index_col=False, sep=" ")
+    binding = data[data.ix[:,2] <= 500]
+    freq = np.zeros((20,9))
+    for line in binding.ix[:,1]:
+        for i, a in enumerate(line):
+            try:
+                freq[int(aa[a]), i] = freq[aa[a],i]+1
+            except KeyError:
+                pass
+    freq_norm = np.true_divide(freq, 409088)
+    freq_norm_r = np.round(freq_norm, 2)
+    aa_df = pd.DataFrame(freq_norm_r, index=[i for i in sorted(aa.keys())], columns=np.arange(1,10))
+    return aa_df
+
+def plot_freq(aa_df, axes):
+#     fig = plt.figure(figsize=(3, 5))
+    ax = sns.heatmap(aa_df, ax = axes)
+#     plt.show()
+
+def plot_entr(input_df, axes):
+    df = pd.DataFrame([stats.entropy(input_df[i], base=2) for i in input_df.columns], columns = ['entropy'])
+    df['position'] = input_df.columns
+    clrs = ['grey' if x > 3 else 'red' for x in df['entropy']]
+#     fig = plt.figure(figsize=(5, 3))
+    sns.barplot(data=df, x='position', y='entropy', palette = clrs, ax = axes)
