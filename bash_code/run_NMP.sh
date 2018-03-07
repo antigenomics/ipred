37c05ec3719d42be4e74ae9cf9b2812108af1711
seaneuron <seaneuron@gmail.com>
2018-03-07 18:31:52 +0300

    dask - disappointment; t-sne kidera - also disappointment'

--- bash_code/run_NMP.sh
+++ bash_code/run_NMP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "netMHCpan prediction has been started to compute. Reading list of arguments" \
  | tee NMP_prediction.log
-readarray -t arguments < new_arguments.txt
+readarray -t arguments < all_arguments.txt
 
 for argument in "${arguments[@]}"; do
-  echo "$argument binding prediction has been started to compute" >&2
+  echo "$argument binding prediction has been started to compute" | tee NMP_prediction.log
   bash NMP_predict.sh -m -a $argument
   echo "Succesfully predicted $argument binding" | tee NMP_prediction.log
   echo "    $argument netMHCpan prediction has been completed!    " >&2
