90b469041e7fc024eff8c606dac7a4dd654a3467
seaneuron <seaneuron@gmail.com>
2019-04-18 14:13:26 +0300

    Added new hydrophobicity scales

--- modules/aa_properties.py
+++ modules/aa_properties.py
@@ -1,5 +1,5 @@
 import pandas
-
+import warnings
 import os
 path, _ = os.path.split(__file__)
 kidera_factors = pandas.read_csv(os.path.join(path, 'kidera.csv'),
@@ -8,6 +8,7 @@ kidera_factors = pandas.read_csv(os.path.join(path, 'kidera.csv'),
 aa_properties = pandas.read_csv(os.path.join(path, 'aa_property_table.csv'),
                                  header=0,
                                  index_col=0)
+aa_hydrophobicity = pandas.read_csv(os.path.join(path, 'hydrophobicity.csv'))
 symbol_lookup = { 'ALA': 'A', 'ARG': 'R',
                   'ASN': 'N', 'ASP': 'D',
                   'CYS': 'C', 'GLN': 'Q',
@@ -23,11 +24,21 @@ kidera_factors.index = kidera_factors.index \
                                      .map(lambda x: symbol_lookup[x])
 aa_properties.index = aa_properties.index \
                                      .map(lambda x: symbol_lookup[x])
+aa_hydrophobicity = aa_hydrophobicity.set_index('aa', drop=True)
+
 
 def score_positions(sequence):
+    warnings.warn(
+            "score_positions will be removed",
+            DeprecationWarning
+        )
     return kidera_factors.loc[list(sequence)]
 
 def score_sequence(sequence, norm=False):
+    warnings.warn(
+            "score_sequence will be removed",
+            DeprecationWarning
+        )
     if norm:
         return kidera_factors.loc[list(sequence)].sum() / len(sequence)
     else:
@@ -39,3 +50,17 @@ def alprop_sequence(sequence, norm=False):
     else:
         return aa_properties.loc[list(sequence)].sum()
 
+def score_hydrophobicity_sequence(sequence, scale="Kyte-Doolittle", norm=False):
+    """
+    Scores hydrophobicity of amino acids with different scales from
+    http://resources.qiagenbioinformatics.com/manuals/clcgenomicsworkbench/650/Hydrophobicity_scales.html
+    :param sequence: your sequence
+    :param scale: One of the following scales:
+        Kyte-Doolittle  Hopp-Woods  Cornette  Eisenberg   Rose  Janin   Engelman GES
+    :param norm: if False returns sum, else mean of the hydrophobicity scores
+    :return: score
+    """
+    if norm:
+        return aa_hydrophobicity.loc[list(sequence), scale].sum() / len(sequence)
+    else:
+        return aa_hydrophobicity.loc[list(sequence), scale].sum()
