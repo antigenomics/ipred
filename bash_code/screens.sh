c81adf1436cdee4775cdc1445afdac91e539617b
seaneuron <seaneuron@gmail.com>
2019-11-26 00:03:24 +0300

    Archived old notebooks

--- bash_code/screens.sh
+++ bash_code/screens.sh
@@ -1,5 +1,1 @@
-screen -d -m -S prediction4 bash -c "python run_ipred1.py -f '../data/fasta/Chlamydia\&trachomatis.fasta' "
-screen -d -m -S prediction5 bash -c "python run_ipred1.py -f '../data/fasta/Clostridium\&difficile.fasta' "
-screen -d -m -S prediction6 bash -c "python run_ipred1.py -f '../data/fasta/Klebsiella\&pneumoniae.fasta' "
-
-
+screen -d -m -S prediction bash -c "python run_ipred1.py -f '../data/fasta/UP000005640_9606.fasta' "
